940ffd83211e2f457b3739d60578afc5a2615bc9
sahithi-2008 <sv0217@srmist.edu.in>
2026-03-03 11:03:42 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,1 +1,10 @@
+public static void main(String[] args) {
 
+    System.out.println("  *****   *****   ******   *****  ");
+    System.out.println(" *     * *     *  *     *  *      ");
+    System.out.println(" *     * *     *  *     *  *      ");
+    System.out.println(" *     * *     *  ******   *****  ");
+    System.out.println(" *     * *     *  *            *  ");
+    System.out.println(" *     * *     *  *            *  ");
+    System.out.println("  *****   *****   *        *****  ");
+}
